2ccf1da76e90e7ca42b985b336a1b61679cd4b87
Ivan Sys <ilsimim@yandex.ru>
2021-09-02 20:01:35 +0300

    First one. Added:  Server init  DB init

---
+++ server/db.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const {Sequelize} = require('sequelize');
+
+module.exports = new Sequelize(
+    process.env.DB_NAME, // Название БД
+    process.env.DB_USER, // Имя пользователя, который запускает БД
+    process.env.DB_PASSWORD, // Пароль пользователя
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT
+    }
+);
